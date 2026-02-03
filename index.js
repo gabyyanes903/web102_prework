dd8efc7dcc884b1b72c487bb928e996f6a7bdce8
gabyyanes903 <gabyyanes456@gmail.com>
2026-02-03 00:03:32 -0500

    Update index.js
    Challenge 7 steps 1 & 2

--- index.js
+++ index.js
@@ -165,7 +165,14 @@ const sortedGames =  GAMES_JSON.sort( (item1, item2) => {
 });
 
 // use destructuring and the spread operator to grab the first and second games
+const [topGame, runnerUpGame, ...rest] = sortedGames;
 
 // create a new element to hold the name of the top pledge game, then append it to the correct element
+const topGameName = document.createElement("h3");
+topGameName.innerHTML = topGame.name;
+firstGameContainer.appendChild(topGameName);
 
 // do the same for the runner up item
+const runnerUpGameName = document.createElement("h3");
+runnerUpGameName.innerHTML = runnerUpGame.name;
+secondGameContainer.appendChild(runnerUpGameName);
